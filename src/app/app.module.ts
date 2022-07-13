798bf34d37392a6f5ae5206002559cc859aa960f
Alexandre Sparton <sparton.alexandre@gmail.com>
2022-07-13 13:10:45 +0200

    tailwindcss added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
+// core modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
+// Primeng components
+import { ButtonModule } from 'primeng/button';
+
+// Developed components
 import { AppComponent } from './app.component';
+
+const routes: Routes = [];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
